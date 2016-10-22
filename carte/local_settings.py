e72f8f158f0d3f96e0d2cfdc7cd7ce4c4c365495
Shashank <shashankagarwal181996@gmail.com>
2016-10-23 03:08:17 +0530

    change database name,user,pasword etc

--- carte/local_settings.py
+++ carte/local_settings.py
@@ -96,14 +96,32 @@ WSGI_APPLICATION = 'carte.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         # 'NAME': os.path.join(BASE_DIR, 'mysql'),
+#         'NAME': 'dea2fprtjcu1ea',
+#         'USER': 'bplhukgsenkrwj',
+#         'PASSWORD': 'SJpOWTlV0S4h7s4gY6iFuoACxa',
+#         'HOST': 'ec2-54-225-100-236.compute-1.amazonaws.com',
+#         'PORT': '5432',
+
+#         # 'NAME': get_env_variable('DATABASE_NAME'),
+#         # 'USER': get_env_variable('DATABASE_USER'),
+#         # 'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+#         # 'HOST': '',
+#         # 'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': os.path.join(BASE_DIR, 'mysql'),
-        'NAME': 'dea2fprtjcu1ea',
-        'USER': 'bplhukgsenkrwj',
-        'PASSWORD': ':SJpOWTlV0S4h7s4gY6iFuoACxa',
-        'HOST': 'ec2-54-225-100-236.compute-1.amazonaws.com',
+        'NAME': 'd3am2ero54f3pb',
+        'USER': 'jaugpoxgvridnd',
+        'PASSWORD': 'HyMGAdcUHmWa1lOXkdDGoCxa7u',
+        'HOST': 'ec2-54-163-245-3.compute-1.amazonaws.com',
         'PORT': '5432',
 
         # 'NAME': get_env_variable('DATABASE_NAME'),
@@ -113,6 +131,8 @@ DATABASES = {
         # 'PORT': '',
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
