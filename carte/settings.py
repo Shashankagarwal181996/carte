b3543de6a84930b8b7713e34d5f34318db09265e
Shashank <shashankagarwal181996@gmail.com>
2016-10-23 16:14:52 +0530

    done login feature

--- carte/settings.py
+++ carte/settings.py
@@ -2,7 +2,7 @@ import dj_database_url
 DATABASES={}
 DATABASES['default'] = dj_database_url.config()
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 SECRET_KEY = '$ep*%(ffh29!z+^fge2sw317j(!)gx7d--la^eb@xtmj&mu!y)'
